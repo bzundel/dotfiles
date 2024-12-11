a2e8e4a86ff2f8645232ad9fb9a0fddb7e7f4a8c
Benedikt Zundel <benedikt.zundel@live.com>
2024-12-11 20:52:32 +0100

    qutebrowser: add some search shortcuts

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,3 +1,9 @@
 config.load_autoconfig(False)
 
 c.tabs.position = "left"
+
+c.url.searchengines = {
+    "DEFAULT":  "https://www.google.com/search?q={}",
+    "!yt":      "https://www.youtube.com/results?search_query={}",
+    "!p":       "https://www.perplexity.ai/search?focus=internet&q={}",
+}
