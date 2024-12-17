406a3b8b08950ffcbaea1ed9540af9b7f83e2207
Benedikt Zundel <benedikt.zundel@live.com>
2024-12-17 05:35:18 +0100

    qutebrowser: tabs bar color

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -2,6 +2,8 @@ config.load_autoconfig(False)
 
 c.tabs.position = "left"
 
+c.colors.tabs.bar.bg = "#000000"
+
 c.url.start_pages = [ "https://www.google.com/" ]
 c.url.searchengines = {
     "DEFAULT":  "https://www.google.com/search?q={}",
