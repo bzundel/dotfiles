2f9f2dbd9549b37a09f6e20895b647ef9c5ebc5e
Benedikt Zundel <benedikt.zundel@bzun.de>
2026-02-07 02:23:13 +0100

    feat(qutebrowser): add gentoo pkg search and mpv bind

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -6,6 +6,9 @@ c.colors.tabs.bar.bg = "#000000"
 
 c.url.start_pages = [ "https://duckduckgo.com/" ]
 c.url.searchengines = {
-    "DEFAULT":  "https://duckduckgo.com/?q={}",
-    "!p":       "https://www.perplexity.ai/search?focus=internet&q={}",
+    "DEFAULT":      "https://duckduckgo.com/?q={}",
+    "!p":           "https://www.perplexity.ai/search?focus=internet&q={}",
+    "!gentoopkg":   "https://packages.gentoo.org/packages/search?q={}"
 }
+
+config.bind("<Ctrl+m>", "hint links spawn mpv {hint-url}")
