ac02fff8743cdd98e6aac53f1711be12020b6c07
Benedikt Zundel <benedikt.zundel@bzun.de>
2026-05-26 10:26:23 +0200

    fix(xmonad): autostart protonmail-bridge

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -57,5 +57,6 @@ definedStartupHook = do
 	spawnOnce "picom"
 	spawnOnce "sxhkd"
 	spawnOnce "dunst"
+	spawnOnce "protonmail-bridge --no-window"
 	spawnOnce "xsetroot -solid \"#000000\""
 	--spawnOnce "feh --bg-fill /usr/share/backgrounds/wallpaper.jpg"
